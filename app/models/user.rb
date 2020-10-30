036f49442f0637e7286f9fc4dd3b57e318f84fb3
fujiwarashi <sur.deux.cordes@gmail.com>
2020-10-31 03:16:31 +0900

    単体テストコードを作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,12 +10,12 @@ class User < ApplicationRecord
   validates :nickname, presence: true
   validates :birthday, presence: true
 
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' } do
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'には全角文字を使用してください' } do
     validates :last_name
     validates :first_name
   end
 
-  with_options presence: true, format: { with: /\A[ァ-ン]+\z/, message: '全角カタカナを使用してください' } do
+  with_options presence: true, format: { with: /\A[ァ-ン]+\z/, message: 'には全角カタカナを使用してください' } do
     validates :last_name_kana
     validates :first_name_kana
   end
